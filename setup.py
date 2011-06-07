c65b9d9beef3adc4c33d6562231e8927682f29e7
Tommy Yu <y@metatoaster.com>
2011-06-07 18:40:12 +1200

    We have a README.md instead.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '0.0'
 setup(name='collective.css3buttons',
       version=version,
       description="Simple framework for creating button links",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.md").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
